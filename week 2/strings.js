caaaa1194e4d725a5d77cf6526f3fb0091b7aff6
Janet-1414 <janetnaturinda5@gmailcom>
2025-08-20 19:52:31 +0300

    adde file

---
+++ week 2/strings.js
@@ -0,0 +1,6 @@
+//strings are data types used to store words
+//They are created by putting in double quotes ("")
+let firstName = "Janet"
+let lastName = "Naturinda"
+let fullName = firstName + " " + lastName
+console.log(firstName, lastName, fullName)
