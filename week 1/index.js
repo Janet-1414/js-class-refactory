e906eb7f01c2bd01a937aeacb2c1fcdcf02e706b
Janet-1414 <janetnaturinda5@gmailcom>
2025-08-20 16:50:46 +0300

    first commit

---
+++ week 1/index.js
@@ -0,0 +1,12 @@
+console.log("hello world!");
+
+let a = 5;
+let b = 3;
+let sum = a + b;
+console.log(sum);
+console.log(a - b);
+
+console.log("my name is Janet");
+console.log("today's class was fun")
+//single line comment//
+/*multiple line comment in js */
---
+++ week 1/variables.js
@@ -0,0 +1,42 @@
+/*primitive data types hold sigle values.
+   1.strings/text
+    2.numbers
+       integers
+       big integers
+       decimals
+    3.Boolean
+        true/ false
+    4.undefines
+    5.null
+      eg.let x = 0
+    6.symbol
+      these are used for uniqueness.*/
+        
+let productName = "timber"
+console.log(typeof(productName))
+let productCost = 3500
+let transportIsNeeded = true
+let tableColor
+let discount = null
+console.log(productCost * 2)
+console.log(productCost/7)
+/*non primitive data types
+1.array [is a list of items]it can consist of words, numbers, symbols etc
+strings are always in quotes.
+
+
+
+2.object in javaScript everything is an object.they are always in carry brackets.
+3.Date
+4.Function*/
+let productList = ["sofa", "table", "bed"];
+console.log(productList)
+let productName1 = "dining table"
+let Quantinty = 2
+let price = 350000
+let totalCost = Quantinty * price
+console.log(totalCost)
+let paymentType = "cash"
+const VAT = 0.05
+
+  
