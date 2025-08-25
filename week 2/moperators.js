f49fcbbd67145105bdeb6f095acef93f8c1e2598
Janet-1414 <janetnaturinda5@gmailcom>
2025-08-25 21:35:18 +0300

    more changes

---
+++ week 2/moperators.js
@@ -0,0 +1,7 @@
+let a = 3;
+let b = 6;
+
+console.log(`a & b = ${a & b} //AND (bits)`);
+console.log(`a | b = ${a | b} //OR (bits)`);
+console.log(`a ^ b = ${a ^ b} //XOR (bits)`);
+console;
---
+++ week 2/numbers.js
@@ -0,0 +1,7 @@
+//NaN is a Javascript reserved word indicating that a number is not a legal numner
+let x = 1000 / "apple"
+console.log(x)
+
+//The maths object is static and all methods and properties can be use without creating a Math Object first
+
+
