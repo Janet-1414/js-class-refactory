6339fccb7afa6e1485ec5c05442a053fc115bfe8
Janet-1414 <janetnaturinda5@gmailcom>
2025-09-02 08:05:39 +0300

    loops

--- week 3/loops.js
+++ week 3/loops.js
@@ -1,12 +1,26 @@
-for (i = 0; i <= 5; i++){
-    console.log(i);
-
+for (i = 0; i <= 5; i++) {
+  console.log(i);
 }
 
 // for (initialization; condition; increment/decrement):for loops
 //initialize; if(condition){code if true}
 //for, for--of, for--in, forEach(), while, do--while
-products = ["chair", "table", "copboard", "wardrobe"]
+const products = ["chair", "table", "copboard", "wardrobe"];
 for (i = 0; i < products.length; i++) {
-    console.log(`items${ i + 1}: ${products[i]}`);
+  console.log(`items${i + 1}: ${products[i]}`);
 }
+
+//while (condition ie stock availability){code if condition is true; increment/decrement}
+let stock = 20;
+while (stock > 0) {
+  console.log(`selling item -- stock left is ${stock}`);
+  stock--;
+}
+
+let payment;
+let totalDue = 10000;
+do {
+  order = Number(prompt(`Enter payment(Total UGX is: ${totalDue}); `));
+} while (payment < totalDue);
+alert("your payment has been received");
+stock--;
